f931cc0346745d974e614e7cb08f1ee605cb4f78
miog54 <olegmavromati@gmail.com>
2023-07-25 12:55:21 +0300

    order

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
     products: [
       {
         productId: {
@@ -15,7 +18,11 @@ const OrderSchema = new mongoose.Schema(
       },
     ],
     amount: { type: Number, required: true },
-    address: { type: Object, required: true },
+    address: { type: String, required: true },
+    apartment: { type: String, required: true },
+    country: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
     status: { type: String, default: "pending" },
   },
   { timestamps: true }
